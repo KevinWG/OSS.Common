5e9907063e502283593c9e4700093b3184cd1d44
王超 <1985088337@qq.com>
2014-10-27 00:27:53 +0800

    修改

--- OS.Common/Models/ResultModel.cs
+++ OS.Common/Models/ResultModel.cs
@@ -3,6 +3,7 @@ namespace OS.Common.Models
 {
     public class ResultModel
     {
+        private int _ret = 200;
         /// <summary>
         /// 返回结果
         ///  2xx   成功相关状态（如： 200）
@@ -11,11 +12,15 @@ namespace OS.Common.Models
         ///  5xx   服务器内部相关错误信息
         ///  6xx   系统级定制错误信息，如升级维护等
         /// </summary>
-        public int ret { get; set; }
+        public int ret
+        {
+            get { return _ret; }
+            set { _ret = value; }
+        }
 
         /// <summary>
         /// 错误或者状态
         /// </summary>
-        public string Message { get; set; }
+        public string message { get; set; }
     }
 }
