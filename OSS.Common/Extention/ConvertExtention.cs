4500ba2b18aa96fcc0dceaf445794131d71bd308
Kevin <1985088337@qq.com>
2017-02-19 20:05:35 +0800

    XmlExtention抽离到单独文件中

--- OSS.Common/Extention/ConvertExtention.cs
+++ OSS.Common/Extention/ConvertExtention.cs
@@ -13,10 +13,7 @@
 #endregion
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Text;
-using System.Xml;
-using System.Xml.Serialization;
 
 namespace OSS.Common.Extention
 {
@@ -563,83 +560,5 @@ namespace OSS.Common.Extention
             return t;
         }
         
-    }
-
-
-    /// <summary>
-    /// xml  转化扩展
-    /// </summary>
-    public static class XmlConvertExtention
-    {
-        /// <summary>
-        /// 对象序列化成xml字符串
-        /// </summary>
-        /// <param name="t"></param>
-        /// <returns></returns>
-        public static string SerializeToXml(this object t)
-        {
-            string result = string.Empty;
-            MemoryStream stream = null;
-            StreamReader streamReader = null;
-            try
-            {
-                var serializer = new XmlSerializer(t.GetType());
-
-                stream = new MemoryStream();
-                serializer.Serialize(stream, t);
-
-                stream.Position = 0;
-                streamReader = new StreamReader(stream);
-                result = streamReader.ReadToEnd();
-            }
-            finally
-            {
-                streamReader?.Dispose();
-                stream?.Dispose();
-            }
-            return result;
-        }
-
-        /// <summary>
-        /// xml字符串转化到对象
-        /// </summary>
-        /// <typeparam name="TT"></typeparam>
-        /// <param name="xml"></param>
-        /// <returns></returns>
-        public static TT DeserializeXml<TT>(this string xml)
-            where TT:class 
-        {
-            return  DeserializeXml(xml, typeof(TT)) as TT;
-        }
-
-        /// <summary>
-        /// xml字符串转化到对象
-        /// </summary>
-        /// <param name="xml"></param>
-        /// <returns></returns>
-        public static object DeserializeXml(this string xml,Type type)
-        {
-            object result = null;
-            MemoryStream stream = null;
-            StreamWriter writer = null;
-            try
-            {
-                stream = new MemoryStream();
-
-                writer = new StreamWriter(stream);
-                writer.Write(xml);
-                writer.Flush();
-
-                stream.Position = 0;
-                var deSer = new XmlSerializer(type);
-                result = deSer.Deserialize(stream);
-            }
-            finally
-            {
-                writer?.Dispose();
-                stream?.Dispose();
-            }
-            return result;
-        }
     }
 }
