e6e17b13c17fc795850c65d5e189bdcc79d17813
Kevin <1985088337@qq.com>
2017-08-27 21:27:13 +0800

    添加配置信息基类

--- OSS.Common/ComModels/AppConfig.cs
+++ OSS.Common/ComModels/AppConfig.cs
@@ -11,6 +11,10 @@
 
 #endregion
 
+using System;
+using System.Threading;
+using OSS.Common.Plugs;
+
 namespace OSS.Common.ComModels
 {
     /// <summary>
@@ -28,4 +32,64 @@ namespace OSS.Common.ComModels
         /// </summary>
         public string AppSecret { get; set; }
     }
+
+    /// <summary>
+    ///   通用配置基类
+    /// </summary>
+    /// <typeparam name="TConfigType"></typeparam>
+    public class BaseConfigProvider<TConfigType>
+        where TConfigType : class, new()
+    {
+
+        #region  接口配置信息
+
+        private static readonly AsyncLocal<TConfigType> _contextConfig = new AsyncLocal<TConfigType>();
+        private readonly TConfigType _config;
+
+        /// <summary>
+        ///  设置上下文配置信息，当前配置在当前上下文中有效
+        /// </summary>
+        /// <param name="config"></param>
+        public static void SetContextConfig(TConfigType config)
+        {
+            _contextConfig.Value = config;
+        }
+
+        /// <summary>
+        /// 微信接口配置
+        /// </summary>
+        public TConfigType ApiConfig
+        {
+            get
+            {
+                if (_config != null)
+                {
+                    return _config;
+                }
+
+                if (_contextConfig.Value != null)
+                {
+                    return _contextConfig.Value;
+                }
+                throw new ArgumentNullException("当前配置信息为空，请通过构造函数中赋值，或者SetContextConfig方法设置当前上下文配置信息");
+            }
+        }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="config"></param>
+        public BaseConfigProvider(TConfigType config = null)
+        {
+            _config = config;
+        }
+
+        #endregion
+
+        /// <summary>
+        ///   当前模块名称
+        /// </summary>
+        public string ModuleName { get; set; } = ModuleNames.Default;
+    }
+
 }
