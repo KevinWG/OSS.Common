a1c3bcfcbfecca65f9c55e08d36a32d2c0c9ecc4
Kevin <1985088337@qq.com>
2017-10-13 19:25:47 +0800

    授权信息字段属性修改

--- OSS.Common/Authrization/SysAuthorizeInfo.cs
+++ OSS.Common/Authrization/SysAuthorizeInfo.cs
@@ -24,6 +24,13 @@ namespace OSS.Common.Authrization
     [Obsolete]
     public class SysAuthorizeInfo : AppAuthorizeInfo
     {
+        #region  字符串处理
+
+        /// <summary>
+        ///   从头字符串中初始化签名相关属性信息
+        /// </summary>
+        /// <param name="signData"></param>
+        /// <param name="separator">A=a  B=b 之间分隔符</param>
         public override void FromSignData(string signData, char separator = ';')
         {
             if (string.IsNullOrEmpty(signData)) return;
@@ -33,7 +40,7 @@ namespace OSS.Common.Authrization
             {
                 if (string.IsNullOrEmpty(str)) continue;
 
-                var keyValue = str.Split(new[] { '=' }, 2);
+                var keyValue = str.Split(new[] {'='}, 2);
                 if (keyValue.Length <= 1) continue;
 
                 var val = keyValue[1].UrlDecode();
@@ -49,7 +56,7 @@ namespace OSS.Common.Authrization
                         AppSource = val;
                         break;
                     case "app_client":
-                        AppClient = val.ToInt32();
+                        AppClient = val;
                         break;
                     case "tenant_id":
                         TenantId = val;
@@ -68,7 +75,7 @@ namespace OSS.Common.Authrization
                         IpAddress = val;
                         break;
                     case "web_browser":
-                        WebBrowser = val.ToInt32();
+                        WebBrowser = val;
                         break;
                     case "pro_code":
                         ProCode = val;
@@ -77,14 +84,18 @@ namespace OSS.Common.Authrization
             }
         }
 
+        #endregion
+        
+        /// <summary>
+        ///   获取要加密签名的串
+        /// </summary>
+        /// <param name="separator"></param>
+        /// <returns></returns>
         protected internal override StringBuilder GetSignContent(char separator)
         {
             var strTicketParas = new StringBuilder();
-            if (AppClient>0)
-            {
-                AddSignDataValue("app_client", AppClient.ToString(), separator, strTicketParas);
-            }
-            
+
+            AddSignDataValue("app_client", AppClient, separator, strTicketParas);
             AddSignDataValue("app_source", AppSource, separator, strTicketParas);
             AddSignDataValue("app_version", AppVersion, separator, strTicketParas);
             AddSignDataValue("device_id", DeviceId, separator, strTicketParas);
@@ -94,12 +105,13 @@ namespace OSS.Common.Authrization
             AddSignDataValue("tenant_id", TenantId, separator, strTicketParas);
             AddSignDataValue("timespan", TimeSpan.ToString(), separator, strTicketParas);
             AddSignDataValue("token", Token, separator, strTicketParas);
-            if (AppClient > 0)
-            {
-                AddSignDataValue("web_browser", WebBrowser.ToString(), separator, strTicketParas);
-            }
+            AddSignDataValue("web_browser", WebBrowser, separator, strTicketParas);
+
             return strTicketParas;
         }
+
+
+
     }
 
     /// <summary>
@@ -121,9 +133,9 @@ namespace OSS.Common.Authrization
 
         /// <summary>
         /// 应用客户端类型
-        /// iOS, Android,PC等用户自定义
+        /// iOS, Android,PC等用户自定义,可以参考 AppClientType
         /// </summary>
-        public int AppClient { get; set; }
+        public string AppClient { get; set; }
 
         /// <summary>
         /// 设备ID
@@ -148,7 +160,7 @@ namespace OSS.Common.Authrization
         /// <summary>
         /// 浏览器类型   可选
         /// </summary>
-        public int WebBrowser { get; set; }
+        public string WebBrowser { get; set; }
 
         /// <summary>
         /// IP地址 可选 手机App端由接收方赋值
@@ -197,7 +209,7 @@ namespace OSS.Common.Authrization
                         AppSource = val;
                         break;
                     case "ac":
-                        AppClient = val.ToInt32();
+                        AppClient = val;
                         break;
                     case "did":
                         DeviceId = val;
@@ -221,7 +233,7 @@ namespace OSS.Common.Authrization
                         Sign = val;
                         break;
                     case "wb":
-                        WebBrowser = val.ToInt32();
+                        WebBrowser = val;
                         break;
                 }
             }
@@ -296,11 +308,8 @@ namespace OSS.Common.Authrization
         protected internal virtual StringBuilder GetSignContent(char separator)
         {
             var strTicketParas = new StringBuilder();
-            if (AppClient>0)
-            {
-                AddSignDataValue("ac", AppClient.ToString(), separator, strTicketParas);
-            }
-          
+
+            AddSignDataValue("ac", AppClient, separator, strTicketParas);
             AddSignDataValue("as", AppSource, separator, strTicketParas);
             AddSignDataValue("av", AppVersion, separator, strTicketParas);
             AddSignDataValue("did", DeviceId, separator, strTicketParas);
@@ -310,10 +319,8 @@ namespace OSS.Common.Authrization
             AddSignDataValue("tid", TenantId, separator, strTicketParas);
             AddSignDataValue("ts", TimeSpan.ToString(), separator, strTicketParas);
             AddSignDataValue("tn", Token, separator, strTicketParas);
-            if (WebBrowser > 0)
-            {
-                AddSignDataValue("wb", WebBrowser.ToString(), separator, strTicketParas);
-            }
+            AddSignDataValue("wb", WebBrowser, separator, strTicketParas);
+
             return strTicketParas;
         }
 
@@ -339,66 +346,66 @@ namespace OSS.Common.Authrization
 
     }
 
-    /// <summary>
-    /// 应用客户端类型
-    /// 1 - 3000  PC
-    /// 3001-6000  Pad  
-    /// 6001- 9000  Mobile
-    /// </summary>
-    public enum AppClientType
-    {
-        /// <summary>
-        ///  未知
-        /// </summary>
-        Unkonw=0,
+    ///// <summary>
+    ///// 应用客户端类型
+    ///// 1 - 3000  PC
+    ///// 3001-6000  Pad  
+    ///// 6001- 9000  Mobile
+    ///// </summary>
+    //public static class AppClientType
+    //{
+    //    /// <summary>
+    //    ///  未知
+    //    /// </summary>
+    //    Unkonw=0,
 
-        /// <summary>
-        /// PC版windows系统
-        /// </summary>
-        Windows = 1,
+    //    /// <summary>
+    //    /// PC版windows系统
+    //    /// </summary>
+    //    Windows = 1,
 
-        /// <summary>
-        ///  PC版苹果系统
-        /// </summary>
-        Macintosh = 30,
+    //    /// <summary>
+    //    ///  PC版苹果系统
+    //    /// </summary>
+    //    Macintosh = 30,
 
-        /// <summary>
-        /// Linux 系统
-        /// </summary>
-        Linux = 60,
+    //    /// <summary>
+    //    /// Linux 系统
+    //    /// </summary>
+    //    Linux = 60,
         
-        //============================= PC 分界线
+    //    //============================= PC 分界线
 
-        /// <summary>
-        ///  Pad泛类
-        /// </summary>
-        Pad=3001,
+    //    /// <summary>
+    //    ///  Pad泛类
+    //    /// </summary>
+    //    Pad=3001,
 
-        /// <summary>
-        ///  苹果pad操作系统
-        /// </summary>
-        iOS_Pad = 3030,
+    //    /// <summary>
+    //    ///  苹果pad操作系统
+    //    /// </summary>
+    //    iOS_Pad = 3030,
 
-        /// <summary>
-        /// 安卓pad端
-        /// </summary>
-        Android_Pad =3060,
+    //    /// <summary>
+    //    /// 安卓pad端
+    //    /// </summary>
+    //    Android_Pad =3060,
         
-        //============================= Pad 分界线
+    //    //============================= Pad 分界线
 
-        /// <summary>
-        ///  手机泛类
-        /// </summary>
-        Mobile= 6001,
+    //    /// <summary>
+    //    ///  手机泛类
+    //    /// </summary>
+    //    Mobile= 6001,
 
-        /// <summary>
-        /// 苹果手机系统
-        /// </summary>
-        iOS = 6030,
+    //    /// <summary>
+    //    /// 苹果手机系统
+    //    /// </summary>
+    //    iOS = 6030,
 
-        /// <summary>
-        /// 安卓手机系统
-        /// </summary>
-        Android=6030
-    }
+    //    /// <summary>
+    //    /// 安卓手机系统
+    //    /// </summary>
+    //    Android=6030
+    //}
 }
