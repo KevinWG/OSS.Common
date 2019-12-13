b456c16d6a993450b6fabd5a1c8f7e10380644a3
KevinW <1985088337@qq.com>
2019-12-13 10:28:42 +0800

    抽离单例和实例容器实现

--- OSS.Common/ComUtils/InsContainer.cs
+++ OSS.Common/ComUtils/InsContainer.cs
@@ -7,19 +7,76 @@ namespace OSS.Common.ComUtils
     /// </summary>
     /// <typeparam name="T"></typeparam>
     public static class InsContainer<T>
-        where T: new()
     {
+        private static Func<T> _insCreater;
+
         /// <summary>
         ///  具体实例 
         /// 根据设置时参数会返回具体单例还是新的实例
         /// </summary>
-        public static T Instance {
+        public static T Instance => _insCreater != null
+            ? _insCreater()
+            : throw new NullReferenceException($"未能发现{typeof(T)}在容器具体映射类型/实例");
+
+
+        /// <summary>
+        ///  设置容器内映射的具体类型
+        /// </summary>
+        /// <typeparam name="TInstance"></typeparam>
+        /// <param name="insCreater"></param>
+        public static void Set<TInstance>(Func<T> insCreater)
+            where TInstance : T
+        {
+            _insCreater = insCreater ?? throw new ArgumentNullException(nameof(insCreater), "参数不能为空！");
+        }
+
+        private static T _instance;
+        private static readonly object _objLock = new object();
+
+        /// <summary>
+        /// 设置容器内映射的具体类型
+        /// </summary>
+        /// <typeparam name="TInstance"></typeparam>
+        /// <param name="isSingle"></param>
+        public static void Set<TInstance>(bool isSingle = true)
+            where TInstance : T, new()
+        {
+            if (!isSingle)
+            {
+                _insCreater = () => new TInstance();
+                return;
+            }
+            _insCreater = () =>
+            {
+                if (_instance == null)
+                {
+                    lock (_objLock)
+                    {
+                        if (_instance == null)
+                        {
+                            return _instance = new TInstance();
+                        }
+                    }
+                }
+                return _instance;
+            };
+        }
+    }
+
+    /// <summary>
+    /// 单例基础实现
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class SingleInstance<T>
+        where T : new()
+    {
+        private static T _instance;
+        private static readonly object _objLock = new object();
+
+        public static T Instance
+        {
             get
             {
-                if (_insCreater != null)
-                {
-                    return _insCreater();
-                }
                 if (_instance == null)
                 {
                     lock (_objLock)
@@ -33,59 +90,5 @@ namespace OSS.Common.ComUtils
                 return _instance;
             }
         }
-
-        private static T _instance;
-        private static Func<T> _insCreater;
-
-        private static readonly object _objLock=new object();
-        /// <summary>
-        ///  设置具体类型映射关系
-        /// </summary>
-        /// <typeparam name="TInstance"></typeparam>
-        /// <param name="insCreater"></param>
-        public static void Set<TInstance>(Func<T> insCreater)
-            where TInstance : T
-        {
-            if (insCreater == null)
-            {
-                throw new ArgumentNullException(nameof(insCreater), "参数不能为空！");
-            }
-            _insCreater = insCreater;
-        }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <typeparam name="TInstance"></typeparam>
-        /// <param name="isSingle"></param>
-        public static void Set<TInstance>(bool isSingle=true)
-            where TInstance : T, new()
-        {
-            if (!isSingle)
-            {
-                _insCreater = () => new TInstance();
-            }
-            else
-            {
-                _insCreater = () =>
-                {
-                    if (_instance == null)
-                    {
-                        lock (_objLock)
-                        {
-                            if (_instance == null)
-                            {
-
-                                return _instance = new TInstance();
-                            }
-                        }
-                    }
-                    return _instance;
-                };
-            }
-          
-        }
-
-
-
     }
 }
