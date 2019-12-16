52d728b1e17b21ac38b80307969cdb3d3eb9179d
KevinW <1985088337@qq.com>
2019-12-16 18:02:00 +0800

    字符串相关扩展方法归类

---
+++ OSS.Common/Extention/StringEncodeExtension.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Text;
+
+namespace OSS.Common.Extention
+{
+    /// <summary>
+    /// url扩展
+    /// </summary>
+    public static class StringEncodeExtension
+    {
+        #region Url 编码处理
+
+        
+        /// <summary>
+        /// Url编码处理
+        /// </summary>
+        public static string UrlEncode(this string input)
+        {
+            return string.IsNullOrEmpty(input)
+                ? string.Empty : Uri.EscapeDataString(input);
+        }
+
+        /// <summary>
+        /// Url解码处理
+        /// </summary>
+        public static string UrlDecode(this string input)
+        {
+            return string.IsNullOrEmpty(input) ? string.Empty : Uri.UnescapeDataString(input);
+        }
+
+        #endregion
+
+
+        #region Base64编码
+        
+        /// <summary>
+        ///    转换字符串为Base64编码
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="encoding"></param>
+        /// <returns></returns>
+        public static string ToBase64(this string source, Encoding encoding = null)
+        {
+            if (string.IsNullOrEmpty(source))
+            {
+                throw new ArgumentNullException(nameof(source), "转化Base64字符串不能为空");
+            }
+
+            if (encoding == null)
+            {
+                encoding = Encoding.UTF8;
+            }
+            var bytes = encoding.GetBytes(source);
+            return Convert.ToBase64String(bytes);
+        }
+
+        /// <summary>
+        ///    解码Base64编码到字符串
+        /// </summary>
+        /// <param name="baseString"></param>
+        /// <param name="encoding"></param>
+        /// <returns></returns>
+        public static string FromBase64(this string baseString, Encoding encoding = null)
+        {
+            if (string.IsNullOrEmpty(baseString))
+            {
+                throw new ArgumentNullException(nameof(baseString), "解码Base64字符串不能为空");
+            }
+            if (encoding == null)
+            {
+                encoding = Encoding.UTF8;
+            }
+            var bytes = Convert.FromBase64String(baseString);
+            return encoding.GetString(bytes);
+        }
+
+
+        /// <summary>
+        ///   转换字符串为Base64编码，同时处理（+，/,=）和url中冲突的字符
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="encoding"></param>
+        /// <returns></returns>
+        public static string ToSafeUrlBase64(this string data, Encoding encoding = null)
+        {
+            return data.ToBase64(encoding).Replace('+', '-').Replace('/', '_').TrimEnd('=');
+        }
+
+
+        /// <summary>
+        ///    解码Base64编码（特殊url冲突处理过的Base64编码）到字符串
+        /// </summary>
+        /// <param name="baseString"></param>
+        /// <param name="encoding"></param>
+        /// <returns></returns>
+        public static string FromSafeUrlBase64(this string baseString, Encoding encoding = null)
+        {
+            var bStr = baseString.Replace('-', '+').Replace('_', '/');
+            var len = bStr.Length % 4;
+            if (len > 0)
+            {
+                bStr += "====".Substring(len);
+            }
+            return bStr.FromBase64(encoding);
+        }
+
+        /// <summary>
+        ///   替换base64位字符串中的特殊符号 Url友好
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        [Obsolete("直接使用ToSafeUrlBase64")]
+        public static string Base64UrlSafeEncode(this string data)
+        {
+            return data.Replace('+', '-').Replace('/', '_').TrimEnd('=');
+        }
+
+        /// <summary>
+        ///   还原 base64 字符串中的特殊字符  Url不友好
+        /// </summary>
+        /// <param name="baseString"></param>
+        /// <returns></returns>
+        [Obsolete("直接使用FromSafeUrlBase64")]
+        public static string Base64UrlSafeDecode(this string baseString)
+        {
+            var bStr= baseString.Replace('-', '+').Replace('_', '/');
+            var len = bStr.Length % 4;
+            if (len > 0)
+            {
+                bStr += "====".Substring(len);
+            }
+            return bStr;
+        }
+        
+        #endregion
+
+    }
+}
--- OSS.Common/Extention/StringNumberExtention.cs
+++ OSS.Common/Extention/StringNumberExtention.cs
@@ -11,18 +11,18 @@
 */
 
 #endregion
+
 using System;
-using System.Text;
 
 namespace OSS.Common.Extention
 {
     /// <summary>
-    /// 
+    /// 字符串数字转化扩展
     /// </summary>
-    public static class ConvertExtention
+    public static class StringNumberExtention
     {
         #region  字符串数字转化部分
-        
+
         /// <summary>
         /// 字符串转化成无符号整形
         /// </summary>
@@ -35,6 +35,7 @@ namespace OSS.Common.Extention
             {
                 return defaultValue;
             }
+
             try
             {
                 if (uint.TryParse(obj, out var returnValue))
@@ -46,10 +47,10 @@ namespace OSS.Common.Extention
             {
                 // ignored
             }
+
             return defaultValue;
         }
-
-
+        
         /// <summary>
         /// 字符串转化成数字
         /// </summary>
@@ -62,6 +63,7 @@ namespace OSS.Common.Extention
             {
                 return defaultValue;
             }
+
             try
             {
                 if (int.TryParse(obj, out var returnValue))
@@ -73,23 +75,10 @@ namespace OSS.Common.Extention
             {
                 // ignored
             }
+
             return defaultValue;
         }
-
-        /// <summary>
-        /// 字符串转化成数字
-        /// </summary>
-        /// <param name="obj">要转化的值</param>
-        /// <param name="defaultValue">如果转化失败，返回的默认值</param>
-        /// <returns></returns>
-        public static int ToInt32(this object obj, int defaultValue = 0)
-        {
-            return obj?.ToString().ToInt32() ?? defaultValue;
-        }
-
-
-
-
+        
         /// <summary>
         /// 字符串转化成数字
         /// </summary>
@@ -102,6 +91,7 @@ namespace OSS.Common.Extention
             {
                 return defaultValue;
             }
+
             try
             {
                 if (long.TryParse(obj, out var returnValue))
@@ -113,6 +103,7 @@ namespace OSS.Common.Extention
             {
                 // ignored
             }
+
             return defaultValue;
         }
 
@@ -130,6 +121,7 @@ namespace OSS.Common.Extention
             {
                 return defaultValue;
             }
+
             try
             {
                 if (decimal.TryParse(obj, out var returnValue))
@@ -141,20 +133,11 @@ namespace OSS.Common.Extention
             {
                 // ignored
             }
+
             return defaultValue;
         }
 
-        /// <summary>
-        /// 字符串转化成数字
-        /// </summary>
-        /// <param name="obj">要转化的值</param>
-        /// <param name="defaultValue">如果转化失败，返回的默认值</param>
-        /// <returns></returns>
-        public static decimal ToDecimal(this object obj, decimal defaultValue = 0)
-        {
-            return obj?.ToString().ToInt32() ?? defaultValue;
-        }
-        
+
         /// <summary>
         /// 字符串转化成数字
         /// </summary>
@@ -167,6 +150,7 @@ namespace OSS.Common.Extention
             {
                 return defaultValue;
             }
+
             try
             {
                 if (double.TryParse(obj, out var returnValue))
@@ -178,20 +162,9 @@ namespace OSS.Common.Extention
             {
                 // ignored
             }
+
             return defaultValue;
         }
-
-        /// <summary>
-        /// 字符串转化成数字
-        /// </summary>
-        /// <param name="obj">要转化的值</param>
-        /// <param name="defaultValue">如果转化失败，返回的默认值</param>
-        /// <returns></returns>
-        public static double ToDouble(this object obj, double defaultValue = 0)
-        {
-            return obj?.ToString().ToDouble() ?? defaultValue;
-        }
-
 
         /// <summary>
         /// 字符串转化成数字
@@ -205,6 +178,7 @@ namespace OSS.Common.Extention
             {
                 return defaultValue;
             }
+
             try
             {
                 if (float.TryParse(obj, out var returnValue))
@@ -216,32 +190,8 @@ namespace OSS.Common.Extention
             {
                 // ignored
             }
+
             return defaultValue;
-        }
-
-        /// <summary>
-        /// 字符串转化成数字
-        /// </summary>
-        /// <param name="obj">要转化的值</param>
-        /// <param name="defaultValue">如果转化失败，返回的默认值</param>
-        /// <returns></returns>
-        public static float ToFloat(this object obj, float defaultValue = 0)
-        {
-            return obj?.ToString().ToFloat() ?? defaultValue;
-        }
-
-        /// <summary>
-        /// 字符串转化成时间
-        /// </summary>
-        /// <param name="obj">要转化的值</param>
-        /// <returns></returns>
-        public static DateTime? ToNullableDateTime(this string obj)
-        {
-            if (string.IsNullOrEmpty(obj))
-            {
-                return null;
-            }
-            return DateTime.TryParse(obj, out var date) ? date : default(DateTime?);
         }
 
         /// <summary>
@@ -254,111 +204,11 @@ namespace OSS.Common.Extention
             {
                 return false;
             }
+
             bool.TryParse(str, out var isOkay);
             return isOkay;
         }
 
-
-
-        /// <summary>
-        ///    转换字符串为Base64编码
-        /// </summary>
-        /// <param name="source"></param>
-        /// <param name="encoding"></param>
-        /// <returns></returns>
-        public static string ToBase64(this string source, Encoding encoding=null)
-        {
-            if (string.IsNullOrEmpty(source))
-            {
-                throw new ArgumentNullException(nameof(source), "转化Base64字符串不能为空");
-            }
-
-            if (encoding==null)
-            {
-                encoding=Encoding.UTF8;
-            }
-            var bytes = encoding.GetBytes(source);
-            return Convert.ToBase64String(bytes);
-        }
-
-        /// <summary>
-        ///    解码Base64编码到字符串
-        /// </summary>
-        /// <param name="baseString"></param>
-        /// <param name="encoding"></param>
-        /// <returns></returns>
-        public static string FromBase64(this string baseString, Encoding encoding=null)
-        {
-            if (string.IsNullOrEmpty(baseString))
-            {
-                throw new ArgumentNullException(nameof(baseString), "解码Base64字符串不能为空");
-            }
-            if (encoding == null)
-            {
-                encoding = Encoding.UTF8;
-            }
-            var bytes = Convert.FromBase64String(baseString);
-            return encoding.GetString(bytes);
-        }
-
-
-        /// <summary>
-        ///   转换字符串为Base64编码，同时处理（+，/,=）和url中冲突的字符
-        /// </summary>
-        /// <param name="data"></param>
-        /// <param name="encoding"></param>
-        /// <returns></returns>
-        public static string ToSafeUrlBase64(this string data, Encoding encoding = null)
-        {
-            return data.ToBase64(encoding).Replace('+', '-').Replace('/', '_').TrimEnd('=');
-        }
-
-
-        /// <summary>
-        ///    解码Base64编码（特殊url冲突处理过的Base64编码）到字符串
-        /// </summary>
-        /// <param name="baseString"></param>
-        /// <param name="encoding"></param>
-        /// <returns></returns>
-        public static string FromSafeUrlBase64(this string baseString, Encoding encoding = null)
-        {
-            var bStr = baseString.Replace('-', '+').Replace('_', '/');
-            var len = bStr.Length % 4;
-            if (len > 0)
-            {
-                bStr +="====".Substring(len);
-            }
-            return bStr.FromBase64(encoding);
-        }
-        
-        /// <summary>
-        ///   替换base64位字符串中的特殊符号 Url友好
-        /// </summary>
-        /// <param name="data"></param>
-        /// <returns></returns>
-        [Obsolete("直接使用ToSafeUrlBase64")]
-        public static string Base64UrlSafeEncode(this string data)
-        {
-            return data.Replace('+', '-').Replace('/', '_').TrimEnd('=');
-        }
-
-        /// <summary>
-        ///   还原 base64 字符串中的特殊字符  Url不友好
-        /// </summary>
-        /// <param name="baseString"></param>
-        /// <returns></returns>
-        [Obsolete("直接使用FromSafeUrlBase64")]
-        public static string Base64UrlSafeDecode(this string baseString)
-        {
-            var bStr= baseString.Replace('-', '+').Replace('_', '/');
-            var len = bStr.Length % 4;
-            if (len>0)
-            {
-                bStr += "====".Substring(len);
-            }
-            return bStr;
-        }
-        
         #endregion
 
         #region Filter
@@ -368,7 +218,8 @@ namespace OSS.Common.Extention
         /// </summary>
         /// <param name="source">传入的字符串</param>
         /// <returns>过滤后的字符串</returns>
-        public static string SqlFilter(string source)
+        [Obsolete]
+        public static string SqlFilter(this string source)
         {
             source = source.Replace("\"", "");
             source = source.Replace("&", "&amp");
