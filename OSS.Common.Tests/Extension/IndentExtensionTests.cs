a41592f9717a04e099bd28d4c70584e344f7a9b9
kevin <1985088337@qq.com>
2021-03-19 13:09:15 +0800

    添加平级列表和递进层级结构列表转化扩展

---
+++ OSS.Common.Tests/Extension/IndentExtensionTests.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using OSS.Common.Extension;
+
+namespace OSS.Common.Tests.Extension
+{
+    [TestClass]
+    public class IndentExtensionTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var flatList = new List<TestFlat>();
+            
+            flatList.Add(new TestFlat() { code = "1", name = "测试1", parnet_code = "0" });
+            flatList.Add(new TestFlat() { code = "2", name = "测试2", parnet_code = "0" });
+            flatList.Add(new TestFlat() { code = "3", name = "测试3", parnet_code = "0" });
+            
+            flatList.Add(new TestFlat() { code = "4", name = "测试4", parnet_code = "0" });
+            flatList.Add(new TestFlat() { code = "11", name = "测试11", parnet_code = "1" });
+            flatList.Add(new TestFlat() { code = "12", name = "测试12", parnet_code = "1" });
+            
+            flatList.Add(new TestFlat() { code = "13", name = "测试13", parnet_code = "1" });
+            flatList.Add(new TestFlat() { code = "21", name = "测试21", parnet_code = "2" });
+            flatList.Add(new TestFlat() { code = "22", name = "测试22", parnet_code = "2" });
+
+
+            var indentlist = flatList.ToIndent<TestFlat,TestIndent>((tf, childrenList) =>
+            {
+                var ti = new TestIndent();
+                ti.name = tf.name;
+                ti.code = tf.code;
+                ti.children = childrenList;
+                return ti;
+            }, tf => tf.parnet_code, tf => tf.code, "0");
+            Assert.IsTrue(indentlist.Count == 4);
+
+            Assert.IsTrue(indentlist[0].children.Count == 3);
+
+            var newFlatList = indentlist.ToFlat((ti) =>
+            {
+                var tFlat = new TestFlat();
+                tFlat.name = ti.name;
+                tFlat.code = ti.code;
+                return tFlat;
+            }, ti => ti.children);
+            Assert.IsTrue(newFlatList.Count == 9);
+        }
+
+    }
+
+
+    public class TestFlat
+    {
+
+        public string name { get; set; }
+
+        public string code { get; set; }
+
+        public string parnet_code { get; set; }
+
+
+    }
+
+    public class TestIndent
+    {
+
+        public string name { get; set; }
+
+        public string code { get; set; }
+
+        public IList<TestIndent> children { get; set; }
+    }
+
+
+
+}
---
+++ OSS.Common/Extension/IndentExtension.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OSS.Common.Extension
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class IndentExtension
+    {
+        /// <summary>
+        /// 平级转化为递进结构
+        /// </summary>
+        /// <typeparam name="TFlat"></typeparam>
+        /// <typeparam name="TIndent"></typeparam>
+        /// <param name="sourceList"></param>
+        /// <param name="convert"></param>
+        /// <param name="getParentColumnValue"></param>
+        /// <param name="getIndentParentColumnValue"></param>
+        /// <param name="defaultParentValue"></param>
+        /// <returns></returns>
+        public static IList<TIndent> ToIndent<TFlat, TIndent>(this IList<TFlat> sourceList,
+           Func<TFlat, IList<TIndent>, TIndent> convert,
+           Func<TFlat, string> getParentColumnValue,
+           Func<TFlat, string> getIndentParentColumnValue,
+           string defaultParentValue)
+        {
+            return sourceList.ToIndent<TFlat, TIndent, string>(convert, getParentColumnValue, getIndentParentColumnValue, defaultParentValue);
+        }
+
+        /// <summary>
+        /// 平级转化为递进结构
+        /// </summary>
+        /// <typeparam name="TFlat"></typeparam>
+        /// <typeparam name="TIndent"></typeparam>
+        /// <typeparam name="TParentVal"></typeparam>
+        /// <param name="sourceList"></param>
+        /// <param name="convert">参数：TFlat实体，子级TIndentList-可能为空，输出：TIndent实体</param>
+        /// <param name="getParentCompareValue"></param>
+        /// <param name="getIndentParentColumnValue"></param>
+        /// <param name="defaultParentValue"></param>
+        /// <returns></returns>
+        public static IList<TIndent> ToIndent<TFlat, TIndent, TParentVal>(this IList<TFlat> sourceList,
+            Func<TFlat,IList<TIndent>, TIndent> convert,
+            Func<TFlat, TParentVal> getParentCompareValue,
+            Func<TFlat, TParentVal> getIndentParentColumnValue,
+            TParentVal defaultParentValue) 
+        {
+            var indentList = new List<TIndent>();
+            foreach (var item in sourceList)
+            {
+                if (getParentCompareValue(item).Equals(defaultParentValue))
+                {
+                    var children= sourceList.ToIndent(convert, getParentCompareValue, getIndentParentColumnValue, getIndentParentColumnValue(item));
+
+                    var indentItem = convert(item, children);
+                    indentList.Add(indentItem);
+                }
+            }
+            return indentList;
+        }
+
+        /// <summary>
+        /// 递进结构转化为平级结构
+        /// </summary>
+        /// <typeparam name="TIndent"></typeparam>
+        /// <typeparam name="TFlat"></typeparam>
+        /// <param name="sourceList"></param>
+        /// <param name="convert"></param>
+        /// <param name="getChildren"></param>
+        /// <returns></returns>
+        public static IList<TFlat> ToFlat<TIndent, TFlat>(this IList<TIndent> sourceList,
+             Func<TIndent,  TFlat> convert,
+             Func<TIndent, IList<TIndent>> getChildren)
+        {
+            var flatList = new List<TFlat>();
+            foreach (var item in sourceList)
+            {
+                var flatItem = convert(item);
+                flatList.Add(flatItem);
+
+                var children = getChildren(item);
+                if (children?.Count>0)
+                {
+                    var childFlatList = children.ToFlat(convert, getChildren);
+                    flatList.AddRange(childFlatList);
+                }
+            }
+            return flatList;
+        }
+    }
+
+
+   
+}
