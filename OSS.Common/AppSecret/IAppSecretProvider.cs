512a1910d8f5b80d23ebe0de2e10bf776277944b
Kevin <1985088337@qq.com>
2022-06-30 22:10:04 +0800

    抽取泛型接口

--- OSS.Common/AppSecret/IAppSecretProvider.cs
+++ OSS.Common/AppSecret/IAppSecretProvider.cs
@@ -6,11 +6,19 @@ namespace OSS.Common;
 /// <summary>
 ///  应用秘钥提供者
 /// </summary>
-public interface IAppSecretProvider
+public interface IAppSecretProvider: IAppSecretProvider<IAppSecret>
+{
+}
+
+/// <summary>
+///  应用秘钥提供者
+/// </summary>
+public interface IAppSecretProvider<TAppSecret>
+    where TAppSecret : IAppSecret
 {
     /// <summary>
-    ///  获取应用秘钥信息
+    /// 获取应用秘钥信息
     /// </summary>
     /// <returns></returns>
-    Task<IResp<IAppSecret>> GetAppSecret();
+    Task<IResp<TAppSecret>> GetAppSecret();
 }
