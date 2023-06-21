b31e521fdf882ae0a700612c9625ac7b9a730434
CC <1985088337@qq.com>
2023-06-21 14:09:40 +0800

    添加Nullable 兼容

--- OSS.Common/Helpers/Extension/StringNumberExtension.cs
+++ OSS.Common/Helpers/Extension/StringNumberExtension.cs
@@ -28,7 +28,7 @@ namespace OSS.Common.Extension
         /// <param name="obj">要转化的值</param>
         /// <param name="defaultValue">如果转化失败，返回的默认值</param>
         /// <returns></returns>
-        public static uint ToUInt32(this string obj, uint defaultValue = 0)
+        public static uint ToUInt32(this string? obj, uint defaultValue = 0)
         {
             if (string.IsNullOrEmpty(obj))
             {
@@ -56,7 +56,7 @@ namespace OSS.Common.Extension
         /// <param name="obj">要转化的值</param>
         /// <param name="defaultValue">如果转化失败，返回的默认值</param>
         /// <returns></returns>
-        public static int ToInt32(this string obj, int defaultValue = 0)
+        public static int ToInt32(this string? obj, int defaultValue = 0)
         {
             if (string.IsNullOrEmpty(obj))
             {
@@ -84,7 +84,7 @@ namespace OSS.Common.Extension
         /// <param name="obj">要转化的值</param>
         /// <param name="defaultValue">如果转化失败，返回的默认值</param>
         /// <returns></returns>
-        public static long ToInt64(this string obj, int defaultValue = 0)
+        public static long ToInt64(this string? obj, int defaultValue = 0)
         {
             if (string.IsNullOrEmpty(obj))
             {
@@ -114,7 +114,7 @@ namespace OSS.Common.Extension
         /// <param name="obj">要转化的值</param>
         /// <param name="defaultValue">如果转化失败，返回的默认值</param>
         /// <returns></returns>
-        public static decimal ToDecimal(this string obj, decimal defaultValue = 0)
+        public static decimal ToDecimal(this string? obj, decimal defaultValue = 0)
         {
             if (string.IsNullOrEmpty(obj))
             {
@@ -143,7 +143,7 @@ namespace OSS.Common.Extension
         /// <param name="obj">要转化的值</param>
         /// <param name="defaultValue">如果转化失败，返回的默认值</param>
         /// <returns></returns>
-        public static double ToDouble(this string obj, double defaultValue = 0)
+        public static double ToDouble(this string? obj, double defaultValue = 0)
         {
             if (string.IsNullOrEmpty(obj))
             {
@@ -171,7 +171,7 @@ namespace OSS.Common.Extension
         /// <param name="obj">要转化的值</param>
         /// <param name="defaultValue">如果转化失败，返回的默认值</param>
         /// <returns></returns>
-        public static float ToFloat(this string obj, float defaultValue = 0)
+        public static float ToFloat(this string? obj, float defaultValue = 0)
         {
             if (string.IsNullOrEmpty(obj))
             {
@@ -192,53 +192,8 @@ namespace OSS.Common.Extension
 
             return defaultValue;
         }
-
-        /// <summary>
-        /// 转化成布尔类型
-        /// </summary>
-        /// <returns></returns>
-        public static bool ToBoolean(this string str)
-        {
-            if (string.IsNullOrEmpty(str))
-            {
-                return false;
-            }
-
-            bool.TryParse(str, out var isOkay);
-            return isOkay;
-        }
-
+        
         #endregion
-
-        #region Filter
-
-        /// <summary>
-        /// 过滤 Sql 语句字符串中的注入脚本
-        /// </summary>
-        /// <param name="source">传入的字符串</param>
-        /// <returns>过滤后的字符串</returns>
-        [Obsolete]
-        public static string SqlFilter(this string source)
-        {
-            source = source.Replace("\"", "");
-            source = source.Replace("&", "&amp");
-            source = source.Replace("<", "&lt");
-            source = source.Replace(">", "&gt");
-            source = source.Replace("delete", "");
-            source = source.Replace("update", "");
-            source = source.Replace("insert", "");
-            source = source.Replace("'", "''");
-            source = source.Replace(";", "；");
-            source = source.Replace("(", "（");
-            source = source.Replace(")", "）");
-            source = source.Replace("Exec", "");
-            source = source.Replace("Execute", "");
-            source = source.Replace("xp_", "x p_");
-            source = source.Replace("sp_", "s p_");
-            source = source.Replace("0x", "0 x");
-            return source;
-        }
-
-        #endregion
+        
     }
 }
