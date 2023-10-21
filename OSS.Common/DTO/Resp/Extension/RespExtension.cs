9650226df60dc42fda062d392588ae18ab320812
CC <1985088337@qq.com>
2023-10-21 10:18:34 +0800

    优化扩展

--- OSS.Common/DTO/Resp/Extension/RespExtension.cs
+++ OSS.Common/DTO/Resp/Extension/RespExtension.cs
@@ -57,6 +57,7 @@
 
         #endregion
 
+
         #region WithResp 基础赋值扩展
 
         /// <summary>
@@ -192,8 +193,23 @@
         }
 
         #endregion
+        
 
         #region 实体相互转化扩展
+
+
+        /// <summary>
+        ///  结果实体转化成异步任务结果实体
+        /// </summary>
+        /// <typeparam name="TRes"></typeparam>
+        /// <param name="res"></param>
+        /// <returns></returns>
+        public static Task<TRes> ToTaskResp<TRes>(this TRes res)
+            where TRes : IResp
+        {
+            return Task.FromResult(res);
+        }
+
 
         /// <summary>
         /// 处理响应转化
@@ -220,8 +236,7 @@
         /// <typeparam name="TRes"></typeparam>
         /// <typeparam name="TPara"></typeparam>
         /// <returns></returns>
-        public static TRes WithResp<TPara, TRes>(this TRes res, TPara tPara, Action<TPara, TRes> successFormatAction,
-            string errMsg = null)
+        public static TRes WithResp<TPara, TRes>(this TRes res, TPara tPara, Action<TPara, TRes> successFormatAction, string errMsg = null)
             where TRes : IResp
             where TPara : IResp
         {
@@ -233,6 +248,30 @@
 
             return string.IsNullOrEmpty(errMsg) ? res : res.WithErrMsg(errMsg);
         }
+
+        /// <summary>
+        /// 处理响应转化
+        /// </summary>
+        /// <param name="targetRes"></param>
+        /// <param name="sourcePara"></param>
+        /// <param name="convertFunc"></param>
+        /// <param name="checkDataNullBeforeConvert">调用方法（convertFunc）之前是否检查数据是否为空</param>
+        /// <typeparam name="TRes"></typeparam>
+        /// <typeparam name="TPara"></typeparam>
+        /// <returns></returns>
+        public static IResp<TRes> WithResp<TRes, TPara>(this IResp<TRes> targetRes, IResp<TPara> sourcePara, Func<TPara, TRes> convertFunc, bool checkDataNullBeforeConvert = true)
+        {
+            if ((checkDataNullBeforeConvert && sourcePara.data != null) || !checkDataNullBeforeConvert)
+            {
+                targetRes.data = convertFunc.Invoke(sourcePara.data);
+            }
+            return targetRes.WithResp(sourcePara);
+        }
+
+        #endregion
+
+
+        #region Data 处理
 
         /// <summary>
         ///  赋值data
@@ -248,68 +287,90 @@
             res.data = data;
             return res;
         }
-
-
-        #region Task 相关扩展
+        
 
         /// <summary>
-        ///  结果实体转化成异步任务结果实体
+        ///  赋值data
         /// </summary>
         /// <typeparam name="TRes"></typeparam>
+        /// <typeparam name="TData"></typeparam>
         /// <param name="res"></param>
+        /// <param name="data"></param>
+        /// <param name="codeWhenDataIsNull">如果Data为空，返回的响应类型</param>
+        /// <param name="msgWhenDataIsNull">如果Data为空，返回的响应消息</param>
         /// <returns></returns>
-        public static Task<TRes> ToTaskResp<TRes>(this TRes res)
-            where TRes : IResp
+        public static TRes WithData<TRes, TData>(this TRes res, TData data, string msgWhenDataIsNull, RespCodes codeWhenDataIsNull = RespCodes.OperateObjectNull)
+            where TRes : IResp<TData>
         {
-            return Task.FromResult(res);
+            return res.WithData(data, msgWhenDataIsNull, (int)codeWhenDataIsNull);
+        }
+
+
+        /// <summary>
+        ///  赋值data
+        /// </summary>
+        /// <typeparam name="TRes"></typeparam>
+        /// <typeparam name="TData"></typeparam>
+        /// <param name="res"></param>
+        /// <param name="data"></param>
+        /// <param name="codeWhenDataIsNull">如果Data为空，返回的响应类型</param>
+        /// <param name="msgWhenDataIsNull">如果Data为空，返回的响应消息</param>
+        /// <returns></returns>
+        public static TRes WithData<TRes, TData>(this TRes res, TData data, string msgWhenDataIsNull, int codeWhenDataIsNull)
+            where TRes : IResp<TData>
+        {
+            res.data = data;
+            if (data == null)
+            {
+                res.WithResp(codeWhenDataIsNull, msgWhenDataIsNull);
+            }
+            return res;
+        }
+
+
+
+
+        /// <summary>
+        ///  赋值data
+        /// </summary>
+        /// <typeparam name="TRes"></typeparam>
+        /// <typeparam name="TData"></typeparam>
+        /// <param name="res"></param>
+        /// <param name="taskData"></param>
+        /// <param name="codeWhenDataIsNull">如果Data为空，返回的响应类型</param>
+        /// <param name="msgWhenDataIsNull">如果Data为空，返回的响应消息</param>
+        /// <returns></returns>
+        public static Task<TRes> WithTaskData<TRes, TData>(this TRes res, Task<TData> taskData, string msgWhenDataIsNull, RespCodes codeWhenDataIsNull = RespCodes.OperateObjectNull)
+            where TRes : IResp<TData>
+        {
+            return res.WithTaskData(taskData, msgWhenDataIsNull,(int)codeWhenDataIsNull);
         }
 
         /// <summary>
-        ///  转化简单Task结果实体
+        ///  赋值data
         /// </summary>
         /// <typeparam name="TRes"></typeparam>
-        /// <param name="resTask"></param>
+        /// <typeparam name="TData"></typeparam>
+        /// <param name="targetRes"></param>
+        /// <param name="taskData"></param>
+        /// <param name="codeWhenDataIsNull">如果Data为空，返回的响应类型</param>
+        /// <param name="msgWhenDataIsNull">如果Data为空，返回的响应消息</param>
         /// <returns></returns>
-        public static async Task<IResp> ToTaskResp<TRes>(this Task<TRes> resTask)
-            where TRes : IResp
+        public static async Task<TRes> WithTaskData<TRes, TData>(this TRes targetRes, Task<TData> taskData, string msgWhenDataIsNull, int codeWhenDataIsNull)
+            where TRes : IResp<TData>
         {
-            return await resTask;
+            targetRes.data = await taskData;
+            if (targetRes.data == null)
+            {
+                targetRes.WithResp(codeWhenDataIsNull, msgWhenDataIsNull);
+            }
+            return targetRes;
         }
-
-        #endregion
         
 
 
-        #region 附带 data 转化处理
-
-        /// <summary>
-        /// 处理响应转化
-        /// </summary>
-        /// <param name="res"></param>
-        /// <param name="tPara"></param>
-        /// <param name="convertFunc"></param>
-        /// <param name="errMsg">如果 tPara.IsSuccess()=false，且errMsg不为空，取errMsg，否则取 tPara.msg </param>
-        /// <param name="isNullCheck">调用方法（convertFunc）之前是否检查数据是否为空</param>
-        /// <typeparam name="TRes"></typeparam>
-        /// <typeparam name="TPara"></typeparam>
-        /// <returns></returns>
-        public static IResp<TRes> WithResp<TRes, TPara>(this IResp<TRes> res, IResp<TPara> tPara,
-                                                        Func<TPara, TRes> convertFunc, string? errMsg=null, bool isNullCheck = true)
-        {
-            if ((isNullCheck && tPara.data != null)
-                || !isNullCheck)
-            {
-                res.data = convertFunc.Invoke(tPara.data);
-            }
-
-            return res.WithResp(tPara, errMsg);
-        }
-
         #endregion
-
-
-        #endregion
-
+        
     }
 
 }
